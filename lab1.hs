ebb6a4531eda0222169f2f8f59df8bc5c17ae4cd
MD-Lai <marvin.dhlai@gmail.com>
2017-08-03 23:15:06 +1000

    question two done

--- lab1.hs
+++ lab1.hs
@@ -1,12 +1,18 @@
 -- Der Hann Marvin Lai 754672 
 -- Optional Project for Comp30020 Declarative Programming
 
--- Function one
+-- Question one
 -- Substitute a value for another in an array of arbitrary with equatable types
-
 subst :: Eq t => t -> t -> [t] -> [t]
 subst _   _   (x : []) = [x]
 subst old new (x : xs)
     |x == old = new : subst old new xs
     |otherwise = x : subst old new xs
 
+-- Question two
+-- Interleave one array into the other
+interleave :: [t] -> [t] -> [t]
+interleave [] [] = []
+interleave [] x  = x
+interleave x []  = x
+interleave (x:xs) (y:ys) = x : y : interleave xs ys
