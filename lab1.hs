5484e272ce9c47a808bf6db468c4baea1a34fced
MD-Lai <marvin.dhlai@gmail.com>
2017-08-03 23:43:48 +1000

    completed question 3 and renamed

--- lab1.hs
+++ lab1.hs
@@ -1,12 +1,16 @@
+module Lab1 (subst, interleave, unroll) where
 -- Der Hann Marvin Lai 754672 
 -- Optional Project for Comp30020 Declarative Programming
+
+-- Useful libs
+--import Data.List
 
 -- Question one
 -- Substitute a value for another in an array of arbitrary with equatable types
 subst :: Eq t => t -> t -> [t] -> [t]
 subst _   _   (x : []) = [x]
 subst old new (x : xs)
-    |x == old = new : subst old new xs
+    |x == old  = new : subst old new xs
     |otherwise = x : subst old new xs
 
 -- Question two
@@ -16,3 +20,10 @@ interleave [] [] = []
 interleave [] x  = x
 interleave x []  = x
 interleave (x:xs) (y:ys) = x : y : interleave xs ys
+
+-- Question three
+-- unrolls an array, i.e. returns array elements repeated n times
+unroll :: Int -> [a] -> [a]
+unroll n x
+    |n <= 0    = []
+    |otherwise = take n x ++ unroll (n - length x) x
